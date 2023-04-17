a18d8e256984880eef4631c4fd4fcb8728b9c98e
albertusfav <favianalbert@gmail.com>
2023-04-17 11:13:27 +0700

    Initial Commit

---
+++ Movie-VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie-VIPER
+//
+//  Created by Albert Mandiri on 17/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
